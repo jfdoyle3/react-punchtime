86a88eed7f96fabdfa0bab09829df13e22f890ec
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-03-04 02:49:49 -0500

    updated add home page and router/routes

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../common/Container';
+
+
+const Home=()=>{
+    return(
+        <Container style={{backgroundColor: "#C9E4CA"}}>
+            <h1>Welcome to Punch Time</h1>
+            <h1>Time Card Management System</h1>
+        </Container>
+    )
+}
+
+export default Home;
